32b3755b3a136f9faa6af8fd391b559f70e51c04
rabume <info@rabu.me>
2022-12-26 20:31:38 +0100

    Added basic content like navbar and footer

--- web/src/components/Body.vue
+++ web/src/components/Body.vue
@@ -1,3 +1,11 @@
 <template>
-    <div>Body</div>
+    <div class="container">
+        <div class="row">
+            <div class="col mt-5">
+                <center>
+                    <img alt="Book and glasses" src="./../assets/book-and-glasses.png" width="350" />
+                </center>
+            </div>
+        </div>
+    </div>
 </template>
--- web/src/components/Footer.vue
+++ web/src/components/Footer.vue
@@ -1,3 +1,14 @@
 <template>
-    <div>Footer</div>
+    <footer class="d-flex align-items-center justify-content-center mt-5">
+        Copyright &copy; {{ new Date().getFullYear() }}
+    </footer>
 </template>
+
+<style scoped>
+footer {
+    background: #212529;
+    color: white;
+    min-height: 3em;
+    font-size: 0.8em;
+}
+</style>
