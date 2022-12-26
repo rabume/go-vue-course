ca6b658d11ae592fdde5e240997caae5462cc663
rabume <info@rabu.me>
2022-12-26 21:07:38 +0100

    Removed center tags

--- web/src/components/Body.vue
+++ web/src/components/Body.vue
@@ -2,9 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col mt-5">
-                <center>
-                    <img alt="Book and glasses" src="./../assets/book-and-glasses.png" width="350" />
-                </center>
+                <img alt="Book and glasses" src="./../assets/book-and-glasses.png" width="350" />
             </div>
         </div>
     </div>
