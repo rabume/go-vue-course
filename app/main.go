5d5173382a1f1ca1643b376a2083580c3c10e3b1
rabume <info@rabu.me>
2022-12-27 15:55:52 +0100

    Included models

--- app/main.go
+++ app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"go-vue-course/internal/data"
 	"go-vue-course/internal/driver"
 	"log"
 	"net/http"
@@ -16,7 +17,7 @@ type application struct {
 	config   config
 	infoLog  *log.Logger
 	errorLog *log.Logger
-	db       *driver.DB
+	models   data.Models
 }
 
 func main() {
@@ -37,7 +38,7 @@ func main() {
 		config:   cfg,
 		infoLog:  infoLog,
 		errorLog: errorLog,
-		db:       db,
+		models:   data.New(db.SQL),
 	}
 
 	err = app.serve()
