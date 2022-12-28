269ff717f2a3e9feaea4b0bc9514c9446b2bad32
rabume <info@rabu.me>
2022-12-28 13:42:15 +0100

    Add stubs for users and books

---
+++ web/src/components/Book.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1 class="mt-3">
+                    Book
+                </h1>
+            </div>
+        </div>
+    </div>
+</template>
