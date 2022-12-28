8097b0693181157f8244a8a1eb13c62f488e0585
rabume <info@rabu.me>
2022-12-28 13:59:41 +0100

    Add navigation links to new pages

--- web/src/components/Header.vue
+++ web/src/components/Header.vue
@@ -12,6 +12,33 @@
                     <li class="nav-item">
                         <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
                     </li>
+
+                    <li class="nav-item">
+                        <router-link class="nav-link active" to="/books">Books</router-link>
+                    </li>
+
+                    <li v-if="store.token !== ''" class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navBarDropDown" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        Admin
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navBarDropDown">
+                        <li>
+                            <router-link class="dropdown-item" to="/admin/users">Manage Users</router-link>
+                        </li>
+                        <li>
+                            <router-link class="dropdown-item" to="/admin/users/0">Add User</router-link>
+                        </li>
+                        <li>
+                            <router-link class="dropdown-item" to="/admin/books">Manage Books</router-link>
+                        </li>
+                        <li>
+                            <router-link class="dropdown-item" :to="{name:'BookEdit', params: {bookId: 0}}">Add Book</router-link>
+                        </li>
+                        </ul>
+
+                    </li>
+
                     <li class="nav-item">
                         <router-link v-if="store.token == ''" class="nav-link" to="/login">Login</router-link>
                         <a href="javascript:void(0);" v-else class="nav-link" @click="logout">Logout</a>
