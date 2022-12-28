3ce72538932acbbc494c850f9674aa41ea19da47
rabume <info@rabu.me>
2022-12-28 14:13:16 +0100

    Add security.js

---
+++ web/src/components/security.js
@@ -0,0 +1,25 @@
+import { store } from './store.js'
+import router from './../router/index.js'
+
+let Security = {
+    // make sure use is authenticated
+    requireToken: function() {
+        if(store.token === '') {
+            router.push("/login");
+            return false;
+        }
+    },
+
+    // Create request options and send them back
+    requestOptions: function(payload) {
+        const headers = new Headers();
+        headers.append("Content-Type", "application/jsonn");
+        headers.append("Authorization", "Bearer " + store.token)
+
+        return {
+            method: "POST",
+            body: JSON.stringify(payload),
+            headers: headers,
+        }
+    }
+}
